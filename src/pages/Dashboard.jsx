86d47013b173d5950ab701f4a891ea7c4bd269cc
Mercy Mwikali <mercymwikali50@gmail.com>
2023-11-13 13:36:34 +0300

    more commits

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,34 +1,31 @@
-import React from 'react'
+import React from 'react';
 import { UserOutlined, ArrowUpOutlined, UsergroupAddOutlined, CreditCardOutlined } from '@ant-design/icons';
 import { Avatar, Card, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
-    <div className='conatiner-fluid'>
-      <div className="row g-3 mx-1 ">
-        <div className="col-md-3 card border-primary  text-center justify-content-center align-items-center py-1">
+    <div className='container overflow-hidden'>
+      <div className="row">
+        <Link to={'/patient-list'} className="col-12 col-md-3 card border-primary justify-content-center align-items-center py-1 mb-3" style={{ backgroundColor: '#67336d' }}>
           <Avatar shape="square" size="large" icon={<UsergroupAddOutlined style={{ color: '#fff' }} />} style={{ backgroundColor: '#67336d' }} />
-          <p className='text-primary py-2 h5'>Today Patient List</p>
-          <p className='h4 text-primary'>125</p>
-        </div>
-        <div className="col-md-3 card border-primary   text-center justify-content-center align-items-center py-2">
+          <p className='text-white py-2 h5'>Today Patient List</p>
+          <p className='h4 text-white'>125</p>
+        </Link>
+        <Link to={'/Inpatient'} className="col-12 col-md-3 card border-primary justify-content-center align-items-center py-1 mb-3 ms-2" style={{ backgroundColor: '#67336d' }}>
           <Avatar shape="square" size="large" icon={<UsergroupAddOutlined style={{ color: '#fff' }} />} style={{ backgroundColor: '#67336d' }} />
-          <p className='text-primary py-2 h5'>Current InPatient List</p>
-          <p className='h4 text-primary'>125</p>
-        </div>
-        <div className="col-md-3 card border-primary text-center justify-content-center align-items-center py-2">
+          <p className='text-white py-2 h5'>Current InPatient List</p>
+          <p className='h4 text-white'>125</p>
+        </Link>
+        <Link to={'/past-doctor-visit'} className="col-12 col-md-3 card border-primary justify-content-center align-items-center py-1 mb-3 ms-2" style={{ backgroundColor: '#67336d' }}>
           <Avatar shape="square" size="large" icon={<UsergroupAddOutlined style={{ color: '#fff' }} />} style={{ backgroundColor: '#67336d' }} />
-          <p className='text-primary py-2 h5'>Doctor's Appointment</p>
-          <p className='h4 text-primary'>125</p>
-        </div>
-        <div className=" col-md-3 card border-primary text-center justify-content-center align-items-center py-2">
-          <Avatar shape="square" size="large" icon={<UsergroupAddOutlined style={{ color: '#fff' }} />} style={{ backgroundColor: '#67336d' }} />
-          <p className='text-primary py-2 h5'>Discharge List</p>
-          <p className='h4 text-primary'>125</p>
-        </div>
+          <p className='text-white py-2 h5'>Doctor's Appointment</p>
+          <p className='h4 text-white'>125</p>
+        </Link>
+       
       </div>
     </div>
   )
 }
 
-export default Dashboard
+export default Dashboard;
